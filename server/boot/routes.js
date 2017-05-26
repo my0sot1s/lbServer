9080dc6a50ffba120d4f8d6cb5be5dce411ee5a3
Nguyễn Mạnh Tể <Te.Nguyen@MacBooks-MacBook-Pro.local>
2017-05-26 15:43:22 +0700

    add demo with image

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -21,6 +21,49 @@ module.exports = function (app) {
     }, 'forecast', req.params.lat, req.params.lon);
   });
 
+  const data = {
+    hat: {
+      link: 'http://i.imgur.com/Fm78yn7.jpg',
+      tag: 'hats',
+      title: 'Hats'
+    },
+    pies: {
+      link: 'http://i.imgur.com/7TTqKsx.jpg',
+      tag: 'pies',
+      title: 'Pies'
+    },
+    coffee: {
+      link: 'http://i.imgur.com/6yMZLA0.jpg',
+      tag: 'coffee',
+      title: 'Coffee'
+    },
+    honey: {
+      link: 'http://i.imgur.com/jj3Y7FE.jpg',
+      tag: 'honey',
+      title: 'Honey'
+    },
+    camera: {
+      link: 'http://i.imgur.com/Q1kqFA0.jpg',
+      tag: 'camera',
+      title: 'Camera'
+    },
+    tree: {
+      link: 'http://i.imgur.com/UFUGlGn.jpg',
+      tag: 'tree',
+      title: 'Tree'
+    },
+    vegetable: {
+      link: 'http://i.imgur.com/PBCuYl6.jpg',
+      tag: 'vegetabe',
+      title: 'Vegetable'
+    },
+  }
+
+  router.get('/natural', (req, res) => {
+    res.send(data)
+  })
+
+
 
   app.use(router);
 }
